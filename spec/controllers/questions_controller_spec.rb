2955d8728b956cf2b29a666bc4c62b674351818f
dd <dd@mb-s.ru>
2018-12-07 02:11:27 +0300

    post

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let (:question) { create(:question)}
   describe 'GET #index' do
-
     let(:questions) {create_list(:question, 3)}
     before { get :index }
     it 'populate an array of all questions' do
@@ -12,5 +12,59 @@ RSpec.describe QuestionsController, type: :controller do
     it 'renders index view' do
       expect(response).to render_template :index
     end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question} }
+    it 'assigns the reqested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+    it 'assigns a new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question} }
+    it 'assigns the reqested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in the database' do
+        expect { post :create, params: {question: attributes_for(:question)} }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: {question: attributes_for(:question)}
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'doesnt save a new question in the database' do
+      end
+
+      it 'redirects to show view'
+    end
+
   end
 end
