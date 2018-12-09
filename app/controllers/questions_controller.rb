65c38276c80534dc6316ffb961e10d54eef2ab9d
dd <dd@mb-s.ru>
2018-12-10 01:55:25 +0300

    finish lesson for question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
   def index
     @questions = Question.all
   end
@@ -17,8 +17,25 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.create(question_params)
-    redirect_to @question
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to question_path
   end
 
   private
@@ -30,3 +47,4 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 end
+ 
