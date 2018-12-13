1db05ce65a6e301d8fbc5a33b0a0d61281857f8d
dd <dmitriy.a.drachev@gmail.com>
2018-12-13 15:56:29 +0300

    rspec. saves a new answer in the database. Tests are green

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,20 @@
 class AnswersController < ApplicationController
+  before_action :load_question, only: [:create]
   def new
     @answer = Answer.new
   end
+
+  def create
+    @answer = @question.answers.create(answer_param)
+  end
+
+  private
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_param
+    params.require(:answer).permit(:body)
+  end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -13,5 +13,32 @@ RSpec.describe AnswersController, type: :controller do
         expect(response).to render_template :new
       end
     end
-  
+ 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: {answer: attributes_for(:answer), question_id: question} }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        #post :create, params: {question: attributes_for(:question)}
+        #expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'doesnt save a new answer in the database' do
+        #expect { post :create, params: {question: attributes_for(:question, :invalid)} }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        #post :create, params: {question: attributes_for(:question, :invalid)}
+        #expect(response).to render_template :new
+      end
+    end
+
+  end
+    
+
+
 end
