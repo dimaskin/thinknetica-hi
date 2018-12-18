947270fabda7de41e7479ba3cd9408f0158c8a5a
dd <dd@mb-s.ru>
2018-12-19 01:10:48 +0300

    add 3 scenario for create question. Test are green

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -29,11 +29,23 @@ feature 'User can create question', %q{
   end
 
   scenario 'Authenticated user asks a question with errors' do
+    visit new_user_session_path
 
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    click_on 'Ask'
+
+    expect(page).to have_content "Title can't be blank"
   end
 
   scenario 'Unauthenticated user tries to asks a question' do
-
-  end
+    visit questions_path
+    click_on 'Ask question'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end                           # You need to sign in or sign up before continuing.
 
 end
