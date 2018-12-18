80ab1cb89c2d41a1faca5b2f5ec473b249720103
dd <dmitriy.a.drachev@gmail.com>
2018-12-18 17:57:09 +0300

    create_spec.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question succefully created.'
     else
       render :new
     end
