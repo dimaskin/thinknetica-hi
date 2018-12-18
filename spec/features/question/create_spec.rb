8ec1234cab538ac2be7dbc44f92a45c2f555b576
dd <dd@mb-s.ru>
2018-12-18 16:31:10 +0300

    new spec question

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'User can create question', %q{
+  In order to get answer from a community
+  As an authenticated user
+  I'd like to be able to sign in
+} do
+
+end
