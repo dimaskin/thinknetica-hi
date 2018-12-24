0702ab6e64ceff7f9ca19876e776181a2cb99d96
dd <dmitriy.a.drachev@gmail.com>
2018-12-24 18:27:04 +0300

    all users can browse all the questions. Test are green

--- spec/features/question/browse_spec.rb
+++ spec/features/question/browse_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 feature 'User can browse all question' do
-  
+  given!(:questions) { create_list :question, 3 }
+    
   describe 'Authenticated user' do
-    given(:questions) { create_list :question, 3 }
-
     given(:user) { create(:user) }
-
     scenario 'Can views all questions' do
+      #question
       visit questions_path
 
       questions.each do |question|
@@ -18,6 +17,12 @@ feature 'User can browse all question' do
 
   end
 
-  scenario 'Unauthenticated user can views all questions'
+  scenario 'Unauthenticated user can views all questions' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content(question.title)
+    end
+  end
 
 end
