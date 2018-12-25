2a6e04ba24f23c1122360dbb4200ee83d73ceb50
dd <dmitriy.a.drachev@gmail.com>
2018-12-25 19:37:47 +0300

    fix create_spec.rb. Alltests are green

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,7 +12,7 @@ class AnswersController < ApplicationController
     if @answer.save
       redirect_to @question, notice: 'Your answer successfully created.'
     else
-      render :new
+      render 'questions/show'
     end
 
   end
