6b49399094d4c1a41fd9b97e0b60e3e58895c074
dd <dmitriy.a.drachev@gmail.com>
2018-12-25 16:25:57 +0300

    add migration UserQuestions, Unauthenticated user can't delete any questions - pass

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  
+  #belongs_to :author, class_name: 'User', foreign_key: :user_id
+  belongs_to :user
   validates :title, :body, presence: true
 end
